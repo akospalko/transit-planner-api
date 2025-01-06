d6710eb0cb2c24c68531f0520545eac978ba9fc6
akospalko <p.akos1148@gmail.com>
2025-01-06 13:40:39 +0100

    Apply user router and body parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import userRouter from "./routes/userRouter";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/v1/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to transit planner api");
