dc341f709b437fe3c30a43e16ee2cc09c59f9205
akospalko <p.akos1148@gmail.com>
2025-01-06 13:39:01 +0100

    Created get all user controller

---
+++ src/controllers/userController/getAllUser.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { prisma } from "../../../prisma/prisma";
+import errorHandlerMiddleware from "../../middleware/errorHandlerMiddleware";
+import sendResponse from "../../utility/responseHandler";
+import { QueriedUserInsensitive } from "../../types/userTypes";
+
+const getAllUsers = errorHandlerMiddleware(
+  async (req: Request, res: Response) => {
+    const totalUsers: number = await prisma.user.count();
+
+    if (totalUsers === 0) {
+      return sendResponse(res, {
+        status: 200,
+        message: "No users present",
+      });
+    }
+
+    const allUsers: QueriedUserInsensitive[] = await prisma.user.findMany({
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        createdAt: true,
+        verifiedAt: true,
+      },
+    });
+
+    return sendResponse<QueriedUserInsensitive[], null>(res, {
+      status: 200,
+      message: "Users retrieved successfully",
+      data: allUsers,
+    });
+  }
+);
+
+export default getAllUsers;
