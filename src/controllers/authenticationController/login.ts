44e36edf9122e1cdb56c43375668e88804b12ae0
akospalko <p.akos1148@gmail.com>
2025-01-09 23:59:47 +0100

    created login controller

---
+++ src/controllers/authenticationController/login.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import errorHandlerMiddleware from "../../middleware/errorHandlerMiddleware";
+import { prisma } from "../../../prisma/prisma";
+import { QueriedUser } from "../../types/userTypes";
+import sendResponse from "../../utility/responseHandler";
+import { ErrorGeneral } from "../../types/ErrorTypes";
+
+export type LoginRequestBody = {
+  // username: string;
+  email: string;
+  password: string;
+};
+
+export const login = errorHandlerMiddleware(
+  async (req: Request, res: Response) => {
+    const { email, /*  username, */ password }: LoginRequestBody = req.body; // TODO username
+
+    const user: QueriedUser | null = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user)
+      return sendResponse<null, ErrorGeneral>(res, {
+        status: 404,
+        error: { general: "User not found" },
+      });
+
+    const isPasswordValid: boolean = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordValid)
+      return sendResponse<null, ErrorGeneral>(res, {
+        status: 401,
+        error: { general: "Invalid credentials" },
+      });
+
+    const accessTokenSecret: string | undefined =
+      process.env.ACCESS_TOKEN_SECRET;
+    const refreshTokenSecret: string | undefined =
+      process.env.REFRESH_TOKEN_SECRET;
+
+    if (!accessTokenSecret || !refreshTokenSecret) {
+      throw new Error("Token secrets are not set in environment variables.");
+    }
+
+    const accessToken: string = jwt.sign(
+      { id: user.id, role: user.role },
+      accessTokenSecret,
+      { expiresIn: "15m" }
+    );
+
+    const refreshToken: string = jwt.sign({ id: user.id }, refreshTokenSecret, {
+      expiresIn: "7d",
+    });
+
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { refreshToken },
+    });
+
+    res.json({ accessToken, refreshToken });
+  }
+);
