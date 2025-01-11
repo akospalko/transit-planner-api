f33fdc84d1a7771a2d5126eb6e437232c53da34b
akospalko <p.akos1148@gmail.com>
2025-01-12 00:45:09 +0100

    Enabled authentication router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import userRouter from "./routes/userRouter";
+import authenticationRouter from "./routes/authenticationRouter";
 import bodyParser from "body-parser";
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/auth", authenticationRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to transit planner api");
