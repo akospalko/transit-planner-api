f9e0f2db21d64e68f4c7ad6f6bc887247c9b883d
akospalko <p.akos1148@gmail.com>
2025-01-12 00:44:57 +0100

    Added routes for login, logout, refresh-token controllers

---
+++ src/routes/authenticationRouter.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import login from "../controllers/authenticationController/login";
+import logout from "../controllers/authenticationController/logout";
+import refreshToken from "../controllers/authenticationController/refreshToken";
+import authenticateToken from "../middleware/authenticateToken";
+// import { Role } from "../enums/authentication";
+// import { authorizeRoles } from "../middleware/authorizeRoles";
+
+const authenticationRouter: Router = Router();
+
+authenticationRouter.post("/login", login);
+authenticationRouter.post("/logout", authenticateToken, logout);
+authenticationRouter.post("/refresh-token", refreshToken);
+
+// TODO
+// authenticationRouter.get(
+//   "/admin",
+//   authenticateToken,
+//   authorizeRoles([Role.ADMIN]),
+//   adminController
+// );
+
+export default authenticationRouter;
