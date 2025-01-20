2ea4d76bc39e4f8c290eca9d4c53b27fa01256f9
akospalko <p.akos1148@gmail.com>
2025-01-20 21:49:18 +0100

    Improved error handler middleare typing. Adjusted data structure

--- src/middleware/errorHandlerMiddleware.ts
+++ src/middleware/errorHandlerMiddleware.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import sendResponse from "../utility/responseHandler";
+import { ErrorResponse } from "../types/ApiTypes";
 
 export type ErrorHandlerMiddlewareParameter = (
   req: Request,
@@ -15,7 +17,14 @@ const errorHandlerMiddleware = (handler: ErrorHandlerMiddlewareParameter) => {
     try {
       await handler(req, res, next);
     } catch (error) {
-      console.error(error);
+      const errors: ErrorResponse<null> = {};
+
+      errors.message = `Error: ${error}`;
+      sendResponse(res, {
+        status: 500,
+        message: "Internal error",
+        error: errors,
+      });
       res.status(500).json({ error: `Error: ${error}` });
     }
   };
