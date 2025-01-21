c04077bfafcb1351afdfbab91ea903c5aa848993
akospalko <p.akos1148@gmail.com>
2025-01-21 19:35:03 +0100

    Adjusted response error handling

--- src/middleware/authorizeRoles.ts
+++ src/middleware/authorizeRoles.ts
@@ -1,17 +1,21 @@
 import { Response, NextFunction } from "express";
 import sendResponse from "../utility/responseHandler";
-import { ErrorGeneral } from "../types/ErrorTypes";
 import { AuthenticatedRequest } from "../types/authenticationTypes";
 import { Role } from "../enums/authentication";
+import { ErrorResponse } from "../types/ApiTypes";
 
 export const authorizeRoles = (roles: Role[]) => {
   return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     const userRoles: Role[] | undefined = req.user?.role;
 
+    const errors: ErrorResponse<null> = {};
+
     if (!userRoles || !roles.some((role) => userRoles.includes(role))) {
-      return sendResponse<null, ErrorGeneral>(res, {
+      errors.message = "Access denied: insufficient permissions";
+      return sendResponse<null, null>(res, {
         status: 403,
-        error: { general: "Access denied: insufficient permissions" },
+        message: "Authorization error",
+        error: errors,
       });
     }
 
