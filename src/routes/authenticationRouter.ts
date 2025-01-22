65602e236a71dc2189916de709b58fd571529dbe
akospalko <p.akos1148@gmail.com>
2025-01-22 22:10:32 +0100

    Removed unused route and related imports

--- src/routes/authenticationRouter.ts
+++ src/routes/authenticationRouter.ts
@@ -4,8 +4,6 @@ import login from "../controllers/authenticationController/login";
 import logout from "../controllers/authenticationController/logout";
 import refreshToken from "../controllers/authenticationController/refreshToken";
 import authenticateToken from "../middleware/authenticateToken";
-// import { Role } from "../enums/authentication";
-// import { authorizeRoles } from "../middleware/authorizeRoles";
 
 const authenticationRouter: Router = Router();
 
@@ -13,13 +11,5 @@ authenticationRouter.post("/register", register);
 authenticationRouter.post("/login", login);
 authenticationRouter.post("/logout", authenticateToken, logout);
 authenticationRouter.post("/refresh-token", refreshToken);
-
-// TODO
-// authenticationRouter.get(
-//   "/admin",
-//   authenticateToken,
-//   authorizeRoles([Role.ADMIN]),
-//   adminController
-// );
 
 export default authenticationRouter;
