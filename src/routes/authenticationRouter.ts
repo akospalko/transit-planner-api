a9e10ae1b5e08b0a307ea289cdb06381c029947d
akospalko <p.akos1148@gmail.com>
2025-02-19 23:31:17 +0100

    Add forgot password and refresh password routes to authentication router

--- src/routes/authenticationRouter.ts
+++ src/routes/authenticationRouter.ts
@@ -3,6 +3,8 @@ import register from "../controllers/authenticationController/register";
 import login from "../controllers/authenticationController/login";
 import logout from "../controllers/authenticationController/logout";
 import refreshToken from "../controllers/authenticationController/refreshToken";
+import forgotPassword from "../controllers/authenticationController/forgotPassword";
+import refreshPassword from "../controllers/authenticationController/refreshPassword";
 import authenticateToken from "../middleware/authenticateToken";
 
 const authenticationRouter: Router = Router();
@@ -11,5 +13,7 @@ authenticationRouter.post("/register", register);
 authenticationRouter.post("/login", login);
 authenticationRouter.post("/logout", authenticateToken, logout);
 authenticationRouter.post("/refresh-token", refreshToken);
+authenticationRouter.post("/forgot-password", forgotPassword);
+authenticationRouter.post("/refresh-password", refreshPassword);
 
 export default authenticationRouter;
