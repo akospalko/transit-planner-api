9f8c9ae100a5be9f60443c33a3ba8ec505a78532
palkoakos <p.akos1148@gmail.com>
2025-02-22 23:42:18 +0100

    Added verify user controller (todo)

---
+++ src/controllers/userController/verifyUser.ts
@@ -0,0 +1,4 @@
+// Flow
+// Requirers: set up emailing
+// 1. via profile ? click btn (profile) ->
+// 2. via email -> generate link -> send link via mail -> click link -> send request -> verify user
