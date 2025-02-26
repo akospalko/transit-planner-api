99d15b831700ac292e67c1cf8c39facfd9fd7aa5
palkoakos <p.akos1148@gmail.com>
2025-02-26 23:33:23 +0100

    Refactor forgot password email link generation and update email template for improved user experience

--- src/controllers/authenticationController/forgotPassword.ts
+++ src/controllers/authenticationController/forgotPassword.ts
@@ -61,7 +61,11 @@ const forgotPassword = errorHandlerMiddleware(
     });
 
     // Send email
-    const resetLink: string = `${process.env.RESET_LINK_PREFIX}/reset-password?token=${resetToken}`;
+    const isDev = process.env.NODE_ENV === "development";
+    const RESET_LINK_PREFIX = isDev
+      ? process.env.RESET_LINK_PREFIX_DEV
+      : process.env.RESET_LINK_PREFIX_PROD;
+    const resetLink: string = `${RESET_LINK_PREFIX}/reset-password?token=${resetToken}`;
 
     // TODO Set up basic email template
     try {
@@ -69,7 +73,12 @@ const forgotPassword = errorHandlerMiddleware(
         from: process.env.EMAIL_USER,
         to: email,
         subject: "Password Reset",
-        text: `Click the link to reset your password: ${resetLink} (available for 15 minutes)`,
+        html: `
+          <p>Click the link below to reset your password:</p>
+          <a href="${resetLink}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #FFF; text-decoration: none; border-radius: 5px;">Reset Password</a>
+          <p>This link is valid for 15 minutes.</p>
+          <p>
+        `,
       });
     } catch (error) {
       return sendResponse<null, null>(res, {
