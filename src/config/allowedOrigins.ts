bb588d6f725181012a158b7f5af877df32334603
palkoakos <p.akos1148@gmail.com>
2025-03-01 23:33:29 +0100

    Update allowed origins to use environment variable for frontend URL

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,11 +1,5 @@
-// TODO Revise required origins
 const allowedOrigins: string[] = [
-  "http://localhost:8081", // Web (browser)
-  "http://192.168.1.76:8081", // Mobile (development IP address)
-  "exp://192.168.1.76:8081", // Mobile URL
-  "http://localhost:8081", // Localhost (web browser testing)
-  "http://localhost:3000",
-  "http://192.168.1.76:3000", // Backend API
+  process.env.FRONTEND_APP_URL || "https://transit-planner.expo.app", //
 ];
 
 export default allowedOrigins;
