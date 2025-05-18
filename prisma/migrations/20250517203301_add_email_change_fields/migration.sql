41851a974a68f668b1d8bb794ff5b22726312510
palkoakos <p.akos1148@gmail.com>
2025-05-18 20:16:35 +0200

    Add email change fields to User model and migration script

---
+++ prisma/migrations/20250517203301_add_email_change_fields/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[pendingEmail]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "emailChangeCode" TEXT,
+ADD COLUMN     "emailChangeExp" TIMESTAMP(3),
+ADD COLUMN     "pendingEmail" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_pendingEmail_key" ON "User"("pendingEmail");
